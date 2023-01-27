f21e10a38e528c6660b76297433755af24da6ce1
Krish Gupta <krshkun@proton.me>
2023-01-28 00:16:20 +0530

    feat: tailwindcss base layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+import partytown from "@astrojs/partytown";
+import image from "@astrojs/image";
+import compress from "astro-compress";
+import critters from "astro-critters";
+import robotdottxt from 'astro-robots-txt';
+
+export default defineConfig({
+	output: "server",
+	adapter: vercel(),
+	integrations: [tailwind(), sitemap(), prefetch(), partytown(), image(), compress(), critters(), robotdottxt()]
+});
