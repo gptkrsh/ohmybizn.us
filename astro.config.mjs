7dcaea4c426baeb82059e010e45a99ce9afc75dd
Krish Gupta <krshkun@proton.me>
2023-01-28 08:59:13 +0530

    feat: done with static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
-import partytown from "@astrojs/partytown";
-import image from "@astrojs/image";
-import compress from "astro-compress";
-import critters from "astro-critters";
-import robotdottxt from 'astro-robots-txt';
+
+// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind(), sitemap(), prefetch(), partytown(), image(), compress(), critters(), robotdottxt(), react()]
+  integrations: [tailwind(), react()]
 });
